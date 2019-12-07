f6c506f664275a92e9caeb869844ecea2dc7abab
Hunter Rocha <47125492+HunterMRocha@users.noreply.github.com>
2019-12-06 17:59:07 -0800

    Add files via upload

---
+++ Hashtable.java
@@ -0,0 +1,153 @@
+import java.util.ArrayList;
+import java.lang.Math;
+public class Hashtable{
+
+	public class HashNode{
+		String key;
+		String value;
+		HashNode next;
+		public HashNode(String key, String value){
+			this.key = key;
+			this.value = value;
+			next = null;
+		}
+	}
+
+	private ArrayList<HashNode> bucketArray;
+
+	// Current capacity of array list
+	private int numBuckets;
+
+	// Current size of array list
+	private int size;
+
+	private float load_threshold = (float)0.5;
+
+	public Hashtable(){
+		bucketArray = new ArrayList<>();
+		numBuckets = 2027;
+		size = 0;
+
+		for(int i=0; i<numBuckets; i++)
+			bucketArray.add(null);
+	}
+
+	public int getSize(){
+		return size;
+	}
+
+	public boolean isEmpty(){
+		if(size==0)
+			return true;
+		return false;
+	}
+
+	public int getHash(String key){
+		return Math.abs(key.hashCode());
+	}
+
+	public int getIndex(String key){
+		return getHash(key) % numBuckets;
+	}
+
+	public boolean containsKey(String key){
+		HashNode head = bucketArray.get(getIndex(key));
+
+		while(head!=null){
+			if(head.key.equals(key))
+				return true;
+			else
+				head = head.next;
+		}
+		return false;
+	}
+
+	public String get(String key){
+		HashNode head = bucketArray.get(getIndex(key));
+
+		while(head!=null){
+			if(head.key.equals(key))
+				return head.value;
+			else
+				head = head.next;
+		}
+
+		return null;
+	}
+
+
+	
+	public void put(String key, String value){
+		int index = getIndex(key);
+		HashNode head = bucketArray.get(index);
+
+		//This changes the value of the HashNode if the key exists
+		while(head != null){
+			if(head.key.equals(key)){
+				head.value = value;
+				return;
+			}
+			head = head.next;
+		}
+
+		head = bucketArray.get(index);
+		HashNode newNode = new HashNode(key, value);
+
+		//Checks if there are no nodes in the index
+		if(head == null){
+			bucketArray.set(index, newNode);
+		}
+
+		else{
+			newNode.next = head;
+			bucketArray.set(index, newNode);
+		}
+
+		size++;
+
+		if((1.0*size)/numBuckets >= load_threshold){
+			ArrayList<HashNode> temp = bucketArray;
+			bucketArray = new ArrayList<>();
+			numBuckets *= 2;
+			size=0;
+
+			for(int i=0; i<numBuckets; i++)
+				bucketArray.add(null);
+
+			for(int i=0; i<temp.size(); i++){
+				HashNode oldNode = temp.get(i);
+				while(oldNode != null){
+					put(oldNode.key, oldNode.value);
+					oldNode = oldNode.next;
+				}
+			}
+		}
+
+	}
+
+	public String remove(String key){
+		int index = getIndex(key);
+		HashNode head = bucketArray.get(index);
+
+		HashNode prev=null;
+		while(head!=null){
+			if(head.key.equals(key))
+				break;
+
+			prev = head;
+			head = head.next;
+		}
+
+		if(head==null)
+			return null;
+
+		size--;
+
+		if(prev!=null)
+			prev.next = head.next;
+		else
+			bucketArray.set(index, head.next);
+		return head.value;
+	}
+	
+}
